91e511e369acd7ba9fff3572bfa5cd2f122c2926
Nadia Elia <nadiaeliamaharani@student.uns.ac.id>
2023-09-28 19:34:19 +0700

    sweetalert

--- resources/views/datapegawai.blade.php
+++ resources/views/datapegawai.blade.php
@@ -7,6 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <title>Data Pegawai</title>
   </head>
@@ -57,7 +58,7 @@
                     <td>{{ $row->mobile }}</td>
                     <td>{{ $row->created_at->diffForHumans() }}</td>
 
-                    <td><a href="" type="button" class="btn btn-danger delete" data-id="{{ $row->id }}">Delete</a>
+                    <td><a href="#" type="button" class="btn btn-danger delete" data-id="{{ $row->id }}" data-name="{{ $row->name }}">Delete</a>
                         <a href="tampilkandata/{{ $row->id }}" type="button" class="btn btn-warning">Edit</a>
                     </td>
                     
@@ -74,19 +75,48 @@
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.7.1.slim.js" integrity="sha256-UgvvN8vBkgO0luPSUl2s8TIlOSYRoGFAX4jlCIm9Adc=" crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
+    
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     -->
   </body>
 
   <script>
-          
+   
+    $('.delete').click(function(){
+      var employeeId = $(this).attr('data-id');
+      var employeeName = $(this).attr('data-name');
+
+      swal({
+  title: "Are you sure?",
+  text: "You will delete employee data by id "+employeeName+" !",
+  icon: "warning",
+  buttons: true,
+  dangerMode: true,
+})
+.then((willDelete) => {
+  if (willDelete) {
+    window.location = "/deletedata/"+employeeId+""
+    swal("Data deleted successfully!", {
+      icon: "success",
+    });
+  } else {
+    swal("Data canceled deleted!");
+  }
+});
+    });
+
+   
+
+  </script>
+
+  <script>
+    toastr.success('Data has been saved successfully!', 'Congrats');
   </script>
 
 </html>
