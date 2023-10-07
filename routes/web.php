17f4a8a96668424cde6c9f55de7392b62b40211d
Nadia Elia <nadiaeliamaharani@student.uns.ac.id>
2023-10-07 15:07:08 +0700

    add middleware

--- routes/web.php
+++ routes/web.php
@@ -24,55 +24,50 @@ Route::get('/', function () {
     $jumlahpegawaifemale = Employee::where('gender','female')->count();
 
     return view('welcome', compact('jumlahpegawai','jumlahpegawaimale','jumlahpegawaifemale'));
-});
+})->middleware('auth');
 
-Route::get('/pegawai',[EmployeeController::class, 'index'])->name('pegawai')->middleware('auth');
 
+
+Route::group(['middleware' => ['auth','hakakses:admin']], function(){
+    //routing pegawai
+    Route::get('/pegawai',[EmployeeController::class, 'index'])->name('pegawai')->middleware('auth');
+    });
+
+
+    
+//search pegawai
+Route::get('/pegawai/search',[EmployeeController::class, 'search'])->name('search');
+
+
+
+//routing crudpegawai
 Route::get('/tambahpegawai',[EmployeeController::class, 'tambahpegawai'])->name('tambahpegawai');
 Route::post('/insertdata',[EmployeeController::class, 'insertdata'])->name('insertdata');
-
 Route::get('/tampilkandata/{id}',[EmployeeController::class, 'tampilkandata'])->name('tampilkandata');
-
 Route::post('/updatedata/{id}',[EmployeeController::class, 'updatedata'])->name('updatedata');
-
 Route::get('/deletedata/{id}',[EmployeeController::class, 'deletedata'])->name('deletedata');
 
+
+
+//import excel
+Route::post('/importexcel',[EmployeeController::class, 'importexcel'])->name('importexcel');
 //export pdf
 Route::get('/exportpdf',[EmployeeController::class, 'exportpdf'])->name('exportpdf');
-
 //export excel
 Route::get('/exportexcel',[EmployeeController::class, 'exportexcel'])->name('exportexcel');
 
 
-//search employee
-Route::get('/pegawai/search',[EmployeeController::class, 'search'])->name('search');
 
-//import excel
-Route::post('/importexcel',[EmployeeController::class, 'importexcel'])->name('importexcel');
-
-
-//register
+//routing login, logout, dan register
 Route::get('/register',[LoginController::class, 'register'])->name('register');
-
-//registerprocess
 Route::post('/registerprocess',[LoginController::class, 'registerprocess'])->name('registerprocess');
-
-
-//login
 Route::get('/login',[LoginController::class, 'login'])->name('login');
-
-//loginprocess
 Route::post('/loginprocess',[LoginController::class, 'loginprocess'])->name('loginprocess');
-
-
-//logout
 Route::get('/logout',[LoginController::class, 'logout'])->name('logout');
 
 
 
-
+//routing agama
 Route::get('/dataagama',[ReligionController::class, 'dataagama'])->name('dataagama')->middleware('auth');
-
 Route::get('/tambahagama',[ReligionController::class, 'tambahagama'])->name('tambahagama');
-
 Route::post('/insertdataagama',[ReligionController::class, 'insertdataagama'])->name('insertdataagama');
